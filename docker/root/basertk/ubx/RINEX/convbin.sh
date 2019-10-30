fb9b11881b92f6f0272c1549c1e6c4811f7a034d
jancelin <julien.ancelin@inra.fr>
2019-10-30 12:01:41 +0100

    mv ubx.tag

--- docker/root/basertk/ubx/RINEX/convbin.sh
+++ docker/root/basertk/ubx/RINEX/convbin.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+set -xv
 cd ./ubx/ &&
 for file in ./*.ubx
 do
   mkdir ./RINEX/${file} &&
   convbin ${file} -v 2.11 -r ubx -od -os -oi -ot -d ./RINEX/${file}/
   mv ${file} ./RINEX/${file}/${file}
+  mv ${file}.tag ./RINEX/${file}/${file}.tag
   cd ./RINEX/${file}
     for f in *.obs
         do cp -- "$f" "${f%.obs}.19o"
