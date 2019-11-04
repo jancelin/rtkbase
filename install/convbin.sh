9b1a7a698c38b63083bef8ad611ed86feb982928
jancelin <julien.ancelin@inra.fr>
2019-11-04 15:09:17 +0100

    change filemename for convbin mv

--- install/convbin.sh
+++ install/convbin.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 #set -xv
 cd ./data/ &&
-for file in ./*.ubx
+for file in *.ubx
 do
-  mkdir ./${file} &&
-  convbin ${file} -v 2.11 -r ubx -od -os -oi -ot -d ./${file}/
-  mv ${file} ./${file}/${file}
-  mv ${file}.tag ./${file}/${file}.tag
-  cd ./${file}
+  filename="${file%.*}"
+  echo ${filename}
+  mkdir -p ./${filename} &&
+  convbin ${file} -v 2.11 -r ubx -od -os -oi -ot -d ./${filename}/
+  mv ./${filename}.ubx ./${filename}/${filename}.ubx
+  mv ./${filename}.ubx.tag ./${filename}/${filename}.tag
+  cd ./${filename}
     for f in *.obs
         do cp -- "$f" "${f%.obs}.19o"
     done 
