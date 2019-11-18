3ee4674205ab5ad9a223b6d7f3185aaede475ff2
jancelin <julien.ancelin@inra.fr>
2019-11-18 17:01:49 +0100

    modify convbin script

--- docker/root/basertk/ubx/RINEX/convbin.sh
+++ docker/root/basertk/ubx/RINEX/convbin.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 #set -xv
-cd ./ubx/ &&
-for file in ./*.ubx
+apt-get install -y zip
+cd ./data/ &&
+for file in $(find . -maxdepth 1 -type f -mmin +60 -name "*.ubx");
+#for file in $(find . -maxdepth 1 -type f -mtime -1 -mmin +60 -name "*.ubx*");
 do
-  mkdir ./RINEX/${file} &&
-  convbin ${file} -v 2.11 -r ubx -od -os -oi -ot -d ./RINEX/${file}/
-  mv ${file} ./RINEX/${file}/${file}
-  mv ${file}.tag ./RINEX/${file}/${file}.tag
-  cd ./RINEX/${file}
-    for f in *.obs
-        do cp -- "$f" "${f%.obs}.19o"
-    done 
-    for z in *.19o
-        do zip -r  "${z}.zip" ${z} &&
-        rm ${z}
-    done
- done
+  #get file name
+  fname="${file%.*}"
+  #conb ubx > RINEX
+  convbin ${file} -v 2.11 -r ubx -od -os -oi -ot -ti 5
+  #change extension & zip obs
+  mv  ${fname}.obs ${fname}.19o
+  zip -m ${fname}.19o.zip ${fname}.19o
+  #zip RINEX directory
+  zip -m ${fname}_5s_rinex2_11 ${fname}.*
+done
--- install/convbin.sh
+++ install/convbin.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 #set -xv
-cd ./data/ &&
-for file in *.ubx
+#This script should be run from a crontab
+#(crontab -l | grep . ; echo -e "0 4 * * * /rtkbase/convbin.sh\n") | crontab -
+#You can customize archive_name and archive_rotate in settings.conf
+
+BASEDIR=$(dirname "$0")
+source ${BASEDIR}/settings.conf
+cd ${datadir}
+
+#archive and compress previous day's gnss data.
+for file in $(find . -maxdepth 1 -type f -mmin +60 -name "*.ubx");
+#for file in $(find . -maxdepth 1 -type f -mtime -1 -mmin +60 -name "*.ubx*");
 do
-  filename="${file%.*}"
-  echo ${filename}
-  mkdir -p ./${filename} &&
-  convbin ${file} -v 2.11 -r ubx -od -os -oi -ot -d ./${filename}/
-  mv ./${filename}.ubx ./${filename}/${filename}.ubx
-  mv ./${filename}.ubx.tag ./${filename}/${filename}.tag
-  cd ./${filename}
-    for f in *.obs
-        do cp -- "$f" "${f%.obs}.19o"
-    done 
-    for z in *.19o
-        do zip -r  "${z}.zip" ${z} &&
-        rm ${z}
-    done
-  cd ..
+  #get file name
+  fname="${file%.*}"
+  #conb ubx > RINEX
+  convbin ${file} -v 2.11 -r ubx -od -os -oi -ot -ti 5
+  #change extension & zip obs
+  mv  ${fname}.obs ${fname}.19o
+  zip -m ${fname}.19o.zip ${fname}.19o
+  #zip RINEX directory
+  zip -m ${fname}_5s_rinex2_11 ${fname}.*
 done
+
+#delete gnss data older than x days.
+find . -maxdepth 1 -type f -name "*.zip" -mtime +${archive_rotate} -delete
