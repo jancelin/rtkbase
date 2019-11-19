bc014c3be11b71e9921983d83be2f20b84681dcb
jancelin <julien.ancelin@inra.fr>
2019-11-19 11:59:38 +0100

    change convbin parameters, working now with cron

--- install/convbin.sh
+++ install/convbin.sh
@@ -2,25 +2,26 @@
 #set -xv
 #This script should be run from a crontab
 #(crontab -l | grep . ; echo -e "0 4 * * * /rtkbase/convbin.sh\n") | crontab -
-#You can customize archive_name and archive_rotate in settings.conf
+#You can customize archive_name, rinex, observ_data, and archive_rotate in settings.conf
 
 BASEDIR=$(dirname "$0")
 source ${BASEDIR}/settings.conf
 cd ${datadir}
 
 #archive and compress previous day's gnss data.
-for file in $(find . -maxdepth 1 -type f -mmin +60 -name "*.ubx");
-#for file in $(find . -maxdepth 1 -type f -mtime -1 -mmin +60 -name "*.ubx*");
+for file in $(find . -maxdepth 1 -type f -mmin +1 -name "*.ubx");
 do
   #get file name
   fname="${file%.*}"
   #conb ubx > RINEX
-  convbin ${file} -v 2.11 -r ubx -od -os -oi -ot -ti 5 &&
+  /usr/local/bin/convbin ${file} -v ${rinex} -r ubx -hm ${mnt_name} -od -os -oi -ot -ti ${observ_data} &&
   #change extension & zip obs
   mv  ${fname}.obs ${fname}.19o &&
-  zip -m ${fname}.19o.zip ${fname}.19o &&
-  #zip RINEX directory
-  zip -m ${fname}_5s_rinex2_11 ${fname}.*
+#When IGN ppp will work
+#  zip -m ${fname}.19o.zip ${fname}.19o && 
+  #zip RINEX & UBX
+  zip -m ${fname}-UBX ${fname}.ubx ${fname}.ubx.tag &&
+  zip -m ${fname}-RINEX_${mnt_name}-${observ_data}s-${file_rotate_time}h ${fname}.*
 done
 
 #delete gnss data older than x days.
