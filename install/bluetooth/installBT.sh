99b2a0cab19cd5232f813caaabaf23995a97dce4
jancelin <julien.ancelin@inra.fr>
2019-11-29 18:26:34 +0100

    add bluetooth nmea

---
+++ install/bluetooth/installBT.sh
@@ -0,0 +1,48 @@
+# https://egjerde.com/blog/?p=66
+
+bluetoothctl
+power on
+discoverable on
+quit
+
+sudo nano /etc/systemd/system/dbus-org.bluez.service
+                                                                                                          
+[Unit]
+Description=Bluetooth service
+Documentation=man:bluetoothd(8)
+ConditionPathIsDirectory=/sys/class/bluetooth
+
+[Service]
+Type=dbus
+BusName=org.bluez
+ExecStart=/usr/lib/bluetooth/bluetoothd -C --noplugin=sap
+ExecStartPost=/usr/bin/sdptool add SP
+NotifyAccess=main
+#WatchdogSec=10
+#Restart=on-failure
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+LimitNPROC=1
+ProtectHome=true
+ProtectSystem=full
+
+[Install]
+WantedBy=bluetooth.target
+Alias=dbus-org.bluez.service
+
+
+sudo nano /etc/systemd/system/rfcomm.service                                                                                                                   
+
+[Unit]
+Description=RFCOMM service
+After=bluetooth.service
+Requires=bluetooth.service
+
+[Service]
+ExecStart=/usr/bin/rfcomm watch hci0 1 getty rfcomm0 115200 vt100 -a root
+
+[Install]
+WantedBy=multi-user.target
+
+
+sudo systemctl enable rfcomm
+sudo systemctl start rfcomm
