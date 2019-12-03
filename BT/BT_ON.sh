841a3423fe1a02a2ef20895022f76c8723885850
jancelin <julien.ancelin@inra.fr>
2019-12-03 17:21:26 +0100

    upd BT

--- BT/BT_ON.sh
+++ BT/BT_ON.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 set -ex
 
-bluetoothctl  << EOF
-  power on
-  discoverable on
-  quit
-EOF
+#bluetoothctl  << EOF
+#  power on
+#  discoverable on
+#  quit
+#EOF
 
-FILE=/dev/rfcomm0
+#FILE=/dev/rfcomm0
 
-while [ ! -d "$file" ]
-do 
-    echo "BT devise Not Connected"
-    sleep 1
-done 
+#while [ ! -d "$file" ]
+#do 
+#    echo "BT devise Not Connected"
+#    sleep 1
+#done 
 sleep 1
-rtkrcv -s -o ./rover/rtkrcv.conf
+rtkrcv -s -o ./rover/rtkrcv.conf << EOF
+status 1
+EOF
